f5a02020e91809778ce53e05b0378b55e29a4a38
Nick Gerner <hello@njgerner.com>
2016-11-30 22:38:22 -0500

    finished afinn model service tests

---
+++ test/models/test-AFINNModelService.js
@@ -0,0 +1,63 @@
+const Promise = require('bluebird');
+const Chai = require('chai');
+const expect = Chai.expect;
+const Config = require('config');
+
+const ArticleTextHelper = require('../helpers/articleTextHelper');
+const GhostArticleSDK = require('../../index');
+const GhostArticleSDKClient = new GhostArticleSDK(Config.get('sdk'));
+const service = GhostArticleSDKClient.AFINNModelService;
+
+describe('AFINNModelService', function () {
+
+  describe('score', () => {
+
+    it('should score an article', () => {
+      const content = ArticleTextHelper.generateArticleContent();
+      return service.score(content)
+      .then(result => {
+        expect(result).to.exist;
+        expect(result).to.be.a('number');
+      })
+    });
+
+    it('should score a html content string', () => {
+      const html = ArticleTextHelper.generateHtmlContent();
+      return service.score(html)
+      .then(result => {
+        expect(result).to.exist;
+        expect(result).to.be.a('number');
+      })
+    });
+
+  });
+
+  describe('scoreEmoji', () => {
+
+    it('should score an article', () => {
+      const content = ArticleTextHelper.generateArticleContent();
+      return service.scoreEmoji(content)
+      .then(result => {
+        expect(result).to.exist;
+        expect(result).to.be.an('object');
+        expect(result).to.contain.keys(['score', 'emoji']);
+        expect(result.score).to.be.a('number');
+        expect(result.emoji).to.be.a('string');
+      })
+    });
+
+    it('should score a html content string', () => {
+      const html = ArticleTextHelper.generateHtmlContent();
+      return service.scoreEmoji(html)
+      .then(result => {
+        expect(result).to.exist;
+        expect(result).to.be.an('object');
+        expect(result).to.contain.keys(['score', 'emoji']);
+        expect(result.score).to.be.a('number');
+        expect(result.emoji).to.be.a('string');
+      })
+    });
+
+  });
+
+});
